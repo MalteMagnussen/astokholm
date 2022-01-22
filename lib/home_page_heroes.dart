de8b93b2fd91c66011b9616b3dad3b9045408b3b
Malte Hviid-Magnussen <maltehviidmagnussen@gmail.com>
2022-01-22 18:23:27 +0100

    Initial data for Heroes

--- lib/home_page_heroes.dart
+++ lib/home_page_heroes.dart
@@ -9,6 +9,24 @@ import 'package:flutter/material.dart';
 class HomePageHeroes extends StatelessWidget {
   const HomePageHeroes({Key? key}) : super(key: key);
 
+  final String sarTitle =
+      "Standalone Synthetic Aperture Radar (SAR) for Sea Ice Charting";
+  final String sarImage = "https://i.imgur.com/xlf4B8p.png";
+
+  final String ganTitle =
+      "Generative Adversarial Networks (GANs) for Sea Ice Charting";
+  final String ganImage = "https://i.imgur.com/ZSGu0Ir.png";
+
+  final String ai4Title = "AI4Arctic Sea Ice Dataset";
+  final String ai4Image = "https://i.imgur.com/GOXDR7g.png";
+
+  final String sentinalTitle =
+      "Sentinel-1 Synthetic Aperture Radar (SAR) interferometric analysis of permafrost thawing cycle in West Greenland";
+  final String sentinalImage = "https://i.imgur.com/e2l7PhH.png";
+
+  final String studentImage1 = "https://i.imgur.com/fXVNHUv.png";
+  final String studentImage2 = "https://i.imgur.com/1pNSNQW.png";
+
   @override
   Widget build(BuildContext context) {
     return Container();
